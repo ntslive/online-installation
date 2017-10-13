1356c6a4a37ee3780ca0524936300201680167cb
Jamie Burke <jamie@ntslive.co.uk>
2017-10-13 18:27:55 +0100

    Initial commit with basic skeleton for project

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        /**
+         * Converts our Sass files to CSS.
+         */
+        sass: {
+            compile: {
+                options: {
+                    style: 'nested'
+                },
+                files: {
+                    'style.css': 'style.scss'
+                }
+            }
+        },
+
+
+        /**
+         * Watches for changes on all JS and CSS files.
+         *
+         * Live reload is working (servers running on localhost 35729 and 35728).
+         * You'll need a Live-reload plugin installed (https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei)
+         *
+         * Changes to grunt file will rerun the grunt script.
+         */
+        watch: {
+            css: {
+                files: ['style.scss'],
+                tasks: ['sass:compile']
+            },
+            configFiles: {
+                files: ['Gruntfile.js'],
+                options: {
+                    reload: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('development', ['sass:compile', 'watch']);
+
+    grunt.registerTask('production',  ['sass:compile']);
+
+};
---
+++ README.md
@@ -0,0 +1,19 @@
+# God Colony
+
+Find Github page for mini-site [here](https://ntslive.github.io/online-installation/).
+
+Created by [NTS Radio](http://www.nts.live).
+
+## Build
+
+Using Sass, which Grunt will compile via tasks 'production' or 'development', that latter of which will continuously run and watch files.
+
+```bash
+npm install
+grunt production
+```
+
+Run local server (http://localhost:3000) with:
+```
+node app.js
+```
